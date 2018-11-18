0aff2ca43adc617a81845b31cc0516116add1a1c
czajnus123 <patzer.daniel@gmail.com>
2018-11-18 08:28:34 +0100

    animation

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public Animator animator;
     private float speed = .1f;
 
     void FixedUpdate()
     {
         Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        animator.SetFloat("Speed", dir.y);
+        animator.SetFloat("SpeedX", dir.x);
+
         Vector2 p = Vector2.MoveTowards(transform.position,
             new Vector2(transform.position.x + dir.x, transform.position.y + dir.y),
             speed);
